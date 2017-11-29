dfff39db34fbd36584aab641eea7fbd96dfa6ec4
GeorgeVaughan <georgio224@gmail.com>
2017-11-29 21:54:25 +0000

    Updated the demo text

--- src/examples/index.jsx
+++ src/examples/index.jsx
@@ -14,13 +14,31 @@ const EXAMPLES = [
     title: "Basic Calendar",
     component: BasicCalendar,
     description:
-      "This is a very basic calendar, simply displaying a month with weekends blocked off, and the option to select a certain day. The other examples shown here are based upon this example.",
-    code: ""
+      "This is a very basic calendar, simply displaying a month with weekends blocked off, and the option to select a certain day. The other examples shown here are based upon this example."
   },
-  { title: "Range Select Calendar", component: RangeSelectCalendar },
-  { title: "Dropdown Calendar", component: DropdownCalendar },
-  { title: "Button Navigation Calendar", component: ButtonNavigationCalendar },
-  { title: "Scroll Calendar", component: ScrollCalendar }
+  {
+    title: "Range Select Calendar",
+    component: RangeSelectCalendar,
+    description:
+      "This component allows a selection of a range of days, and shows the use of the 'modifiers' prop."
+  },
+  {
+    title: "Dropdown Calendar",
+    component: DropdownCalendar,
+    description:
+      "This component shows how the component could be used in conjunction with an <input/> element."
+  },
+  {
+    title: "Button Navigation Calendar",
+    component: ButtonNavigationCalendar,
+    description:
+      "This component shows one of the simpler ways to create a navigational calender, with two buttons to slide backwards and forwards by a month."
+  },
+  {
+    title: "Scroll Calendar",
+    component: ScrollCalendar,
+    description: "This component shows a calendar that is scrollable."
+  }
 ];
 
 export default () => (
@@ -31,7 +49,6 @@ export default () => (
         <h2>{title}</h2>
         <div className="component">{React.createElement(component)}</div>
         <div className="description">{description}</div>
-        <div className="code">Code: {code}</div>
       </div>
     ))}
   </div>
