0b695055019d2ae869c98b93cd30e051fb03fc99
GeorgeVaughan <georgio224@gmail.com>
2018-01-28 15:04:15 +0000

    Added component utils to exports.js

--- src/exports.js
+++ src/exports.js
@@ -5,3 +5,12 @@ export { default as MonthGrid } from "./components/MonthGrid";
 export { default as MonthTitle } from "./components/MonthTitle";
 export { default as Week } from "./components/Week";
 export { default as WeekHeader } from "./components/WeekHeader";
+
+export {
+  default as ComponentWrappers
+} from "./components/utils/ComponentWrappers";
+export { default as ArrayHelpers } from "./components/utils/ArrayHelpers";
+export { default as Constants } from "./components/utils/Constants";
+export { default as Modifiers } from "./components/utils/Modifiers";
+export { default as MomentHelpers } from "./components/utils/MomentHelpers";
+export { default as PropHelpers } from "./components/utils/PropHelpers";
