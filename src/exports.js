cd63f706346c58fc433c1b97b3d455fd7baa9996
GeorgeVaughan <georgio224@gmail.com>
2018-02-11 10:59:05 +0000

    Fixed exports.js better

--- src/exports.js
+++ src/exports.js
@@ -5,21 +5,11 @@ export { default as MonthGrid } from "./components/MonthGrid";
 export { default as MonthTitle } from "./components/MonthTitle";
 export { default as Week } from "./components/Week";
 export { default as WeekHeader } from "./components/WeekHeader";
+export { default as Popup } from "./components/Popup";
 
-import * as ComponentWrappers from "./components/utils/ComponentWrappers";
-export { ComponentWrappers };
-
-import * as ArrayHelpers from "./components/utils/ArrayHelpers";
-export { ArrayHelpers };
-
-import * as Constants from "./components/utils/Constants";
-export { Constants };
-
-import * as Modifiers from "./components/utils/Modifiers";
-export { Modifiers };
-
-import * as MomentHelpers from "./components/utils/MomentHelpers";
-export { MomentHelpers };
-
-import * as PropHelpers from "./components/utils/PropHelpers";
-export { PropHelpers };
+export * from "./components/utils/ComponentWrappers";
+export * from "./components/utils/ArrayHelpers";
+export * from "./components/utils/Constants";
+export * from "./components/utils/Modifiers";
+export * from "./components/utils/MomentHelpers";
+export * from "./components/utils/PropHelpers";
