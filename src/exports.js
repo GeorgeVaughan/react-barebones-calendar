1779c0dfcc32d9b9abf4259975060cb98bb82cd6
GeorgeVaughan <georgio224@gmail.com>
2018-02-11 10:49:54 +0000

    Fixed exports.js not exporting utils

--- src/exports.js
+++ src/exports.js
@@ -6,11 +6,20 @@ export { default as MonthTitle } from "./components/MonthTitle";
 export { default as Week } from "./components/Week";
 export { default as WeekHeader } from "./components/WeekHeader";
 
-export {
-  default as ComponentWrappers
-} from "./components/utils/ComponentWrappers";
-export { default as ArrayHelpers } from "./components/utils/ArrayHelpers";
-export { default as Constants } from "./components/utils/Constants";
-export { default as Modifiers } from "./components/utils/Modifiers";
-export { default as MomentHelpers } from "./components/utils/MomentHelpers";
-export { default as PropHelpers } from "./components/utils/PropHelpers";
+import * as ComponentWrappers from "./components/utils/ComponentWrappers";
+export { ComponentWrappers };
+
+import * as ArrayHelpers from "./components/utils/ArrayHelpers";
+export { ArrayHelpers };
+
+import * as Constants from "./components/utils/Constants";
+export { Constants };
+
+import * as Modifiers from "./components/utils/Modifiers";
+export { Modifiers };
+
+import * as MomentHelpers from "./components/utils/MomentHelpers";
+export { MomentHelpers };
+
+import * as PropHelpers from "./components/utils/PropHelpers";
+export { PropHelpers };
